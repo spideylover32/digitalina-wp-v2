48dfe0b0e4e65b1b8da8cf05d6a86e90aa9dd019
xpander54 <r.zepeda84@gmail.com>
2013-10-18 21:30:56 -0500

    sync conf

--- wp-config.php
+++ wp-config.php
@@ -16,10 +16,10 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wp-digitalina-db');
+define('DB_NAME', 'wp_digitalina_db');
 
 /** MySQL database username */
-define('DB_USER', 'digitalina-sql');
+define('DB_USER', 'digitalina_sql');
 
 /** MySQL database password */
 define('DB_PASSWORD', 'qd82Ty8V');
